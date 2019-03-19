052b80256c0047af0c97c6b78c1115c47934d39a
Ernesto Bonet Moncada <bernestoalberto@gmail.com>
2019-03-19 13:42:30 -0400

    Initial

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,13 +1,13 @@
-pragma solidity 0.4.24;
+pragma solidity 0.5.0;
 
 contract SimpleStorage {
   string ipfsHash;
 
-  function set(string x) public {
+  function set(string memory x) public {
     ipfsHash = x;
   }
 
-  function get() public view returns (string) {
+  function get() public view returns (string memory) {
     return ipfsHash;
   }
 }
